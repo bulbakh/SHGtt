38af28d1f426ac4d0f094afeb2c68b9eadf9ea48
bulbakh <bulbakh@gmail.com>
2024-10-31 19:51:25 +0200

    documentation added

--- app/Models/LoggerFactory.php
+++ app/Models/LoggerFactory.php
@@ -1,20 +1,27 @@
 <?php
+
 namespace App\Models;
 
 use App\Interfaces\LoggerInterface;
 use App\Models\Loggers\EmailLogger;
 use App\Models\Loggers\DbLogger;
 use App\Models\Loggers\FileLogger;
+use InvalidArgumentException;
 
 class LoggerFactory
 {
+    /**
+     * @param string $type
+     * @return LoggerInterface
+     * @throws InvalidArgumentException
+     */
     public static function create(string $type): LoggerInterface
     {
         return match ($type) {
             'email' => new EmailLogger(),
             'db' => new DbLogger(),
             'file' => new FileLogger(),
-            default => throw new \InvalidArgumentException("Invalid logger type «{$type}»."),
+            default => throw new InvalidArgumentException("Invalid logger type «{$type}»."),
         };
     }
 
